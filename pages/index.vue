9f14f8f198f44aebb4c671d48203cad21f6e6e11
MO910 <33283801+MO910@users.noreply.github.com>
2022-12-14 14:37:45 +0200

    temp

--- pages/index.vue
+++ pages/index.vue
@@ -4,25 +4,25 @@
         <v-card width="400">
             <v-card-title>This is a title</v-card-title>
             <v-card-subtitle>This is a subtitle</v-card-subtitle>
-            <v-card-text> This is {{ data }} </v-card-text>
+            <v-card-text> This is {{ dataa }} </v-card-text>
         </v-card>
     </div>
 </template>
 
 <script setup>
 const counter = useState("counter", () => Math.round(Math.random() * 1000));
-const dataa = await useFetch(`/api/mongo`, {});
+const dataa = await useFetch(`/api/mongo`);
 
-const query = gql`
-    query characters {
-        characters {
-            results {
-                name
-                gender
-            }
-        }
-    }
-`;
-const { data } = await useAsyncQuery(query);
-// console.log(dataa);
+// const query = gql`
+//     query characters {
+//         characters {
+//             results {
+//                 name
+//                 gender
+//             }
+//         }
+//     }
+// `;
+// const { data } = await useAsyncQuery(query);
+console.log(dataa);
 </script>
